af84d28afae648c65704594ce5806388193216a5
Itagawa Yuki <40556606+shi3osi123456@users.noreply.github.com>
2019-08-08 01:24:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,18 +2,21 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+@client.event
+async def on_message(message):
+    if message.content.startswith('/join'):
+        role = discord.utils.get(message.guild.roles, name='member')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} ようこそ！'
+        await message.channel.send(reply)
 
 bot.run(token)
